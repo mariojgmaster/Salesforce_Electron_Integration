bad03fda80158e934a227c706169c17f57c941e4
mariojgmaster <mariojgmaster@yahoo.com.br>
2022-05-01 20:55:29 -0300

    initializing

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+    * @description      : 
+    * @author           : mario
+    * @group            : 
+    * @created          : 01/05/2022 - 20:54:30
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 01/05/2022
+    * - Author          : mario
+    * - Modification    : 
+**/
